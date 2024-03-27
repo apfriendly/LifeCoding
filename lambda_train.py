7b2a80d3563955f4bd3c5a33e0742bdd6efaa323
FriendlyApp <163114706+apfriendly@users.noreply.github.com>
2024-03-27 14:35:21 +0200

    Update 2 lambda_train.py

--- lambda_train.py
+++ lambda_train.py
@@ -6,3 +6,12 @@ words = ["apple", "banana", "cherry", "date", "fig"]
 get_first_letter = lambda x: x[0].upper()
 first_letters = list(map(get_first_letter, words))
 print(first_letters)
+
+
+# Напиши лямбда-выражение, которое принимает букву и возвращает количество слов в списке, содержащих эту букву.
+# Например, для буквы "a" результат должен быть 3, потому что в списке есть слова "apple", "banana" и "date".
+
+words = ["apple", "banana", "cherry", "date", "fig"]
+letter = input()
+count = len(list(filter(lambda word: letter in word, words)))
+print(count)
