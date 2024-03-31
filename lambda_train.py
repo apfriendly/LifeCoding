96034913b42b25df8de100b795994669eadc2fde
FriendlyApp <163114706+apfriendly@users.noreply.github.com>
2024-03-31 20:46:54 +0300

    Update 6 lambda_train.py

--- lambda_train.py
+++ lambda_train.py
@@ -38,3 +38,11 @@ print(upper_word)
 items = [("book", 20), ("pen", 5), ("pencil", 2), ("notebook", 10)]
 sorted_items = list(sorted(items, key=lambda item: item[1]))
 print(sorted_items)
+
+
+# Напиши код, используя лямбда-выражение, чтобы найти максимальное число в списке
+# Подсказка: используй функцию max() и передай ей ключ (key) с лямбда-выражением
+#
+numbers = [10, 5, 25, 15, 30, 20]
+maximum = lambda x: max(x)
+print(maximum(numbers))
